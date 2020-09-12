ca142912932c304a93c8f5c9f3f1365db17a8944
lpetsoan@student.wethinkcode.co.za <lpetsoan@student.wethinkcode.co.za>
2020-09-12 23:30:51 +0200

    Short break

---
+++ swingyApp/src/main/java/com/lpetsoan/wtc/View/GUI/Gui.java
@@ -0,0 +1,10 @@
+package com.lpetsoan.wtc.View.GUI;
+
+import com.lpetsoan.wtc.View.View;
+
+public class Gui implements View {
+    @Override
+    public void start() {
+        System.out.println("The gui view getting started");
+    }
+}
